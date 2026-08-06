6f11a3f5591c6d0e3f1791f9fd8bb516857a209b
jeffreykeddaue5 <jeffreykeddaue5@gmail.com>
2026-08-06 17:34:25 -0400

    Adds and implements widget switcher

--- Plugins/InventoryPlugin/Source/Widgets/Private/SpatialInventory.cpp
+++ Plugins/InventoryPlugin/Source/Widgets/Private/SpatialInventory.cpp
@@ -2,3 +2,48 @@
 
 
 #include "SpatialInventory.h"
+
+#include "InventoryGrid.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+
+void USpatialInventory::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+	ButtonEquippables->OnClicked.AddDynamic(this, &USpatialInventory::ShowEquippables);
+	ButtonConsumables->OnClicked.AddDynamic(this, &USpatialInventory::ShowConsumables);
+	ButtonCraftables->OnClicked.AddDynamic(this, &USpatialInventory::ShowCraftables);
+	
+	ShowEquippables();
+}
+
+void USpatialInventory::ShowEquippables()
+{
+	SetActiveGrid(GridEquippables, ButtonEquippables);
+}
+
+void USpatialInventory::ShowConsumables()
+{
+	SetActiveGrid(GridConsumables, ButtonConsumables);
+}
+
+void USpatialInventory::ShowCraftables()
+{
+	SetActiveGrid(GridCraftables, ButtonCraftables);
+}
+
+void USpatialInventory::DisableButton(UButton* Button)
+{
+	ButtonEquippables->SetIsEnabled(true);
+	ButtonConsumables->SetIsEnabled(true);
+	ButtonCraftables->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void USpatialInventory::SetActiveGrid(UInventoryGrid* Grid, UButton* Button)
+{
+	DisableButton(Button);
+	GridSwitcher->SetActiveWidget(Grid);
+	
+}
--- Plugins/InventoryPlugin/Source/Widgets/Public/SpatialInventory.h
+++ Plugins/InventoryPlugin/Source/Widgets/Public/SpatialInventory.h
@@ -6,6 +6,7 @@
 #include "InventoryBase.h"
 #include "SpatialInventory.generated.h"
 
+class UButton;
 class UInventoryGrid;
 class UWidgetSwitcher;
 /**
@@ -16,10 +17,13 @@ class WIDGETS_API USpatialInventory : public UInventoryBase
 {
 	GENERATED_BODY()
 	
+	public:
+	virtual void NativeOnInitialized() override;
+	
 private:
 	
 	UPROPERTY(meta=(BindWidget))
-	TObjectPtr<UWidgetSwitcher> WidgetSwitcher;
+	TObjectPtr<UWidgetSwitcher> GridSwitcher;
 	
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInventoryGrid> GridEquippables;
@@ -30,4 +34,24 @@ private:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInventoryGrid> GridCraftables;
 	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ButtonEquippables;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ButtonConsumables;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ButtonCraftables;
+	
+	UFUNCTION()
+	void ShowEquippables();
+	
+	UFUNCTION()
+	void ShowConsumables();
+	
+	UFUNCTION()
+	void ShowCraftables();
+	void DisableButton(UButton* Button);
+
+	void SetActiveGrid(UInventoryGrid* Grid, UButton* Button);
 };
